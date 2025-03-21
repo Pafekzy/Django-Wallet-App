5e7bad9ef8b0486d6781a0bbd892287cf7217452
github.com/Pafekzy <github.com/Pafekzy.gmail.com>
2025-03-21 12:04:35 +0100

    Encountered A Sign Up Bug!

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from .views import home
 
 urlpatterns = [
     path("", home, name="home"),  # Root URL
+    path("signup/", signup, name="signup"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-def home(request):
-    return render(request, "home.html")  # Make sure this file exists in templates
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after signing up
+            return redirect("home")  # Redirect to home page
+    else:
+        form = UserCreationForm()
+
+    return render(request, "core/signup.html", {"form": form})
